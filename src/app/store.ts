f50820cac43d1381906bab5beca97c31bfd8d2d2
M <hozhabr.workplace@gmail.com>
2022-11-05 15:58:17 +0330

    Fix: eslint

--- src/app/store.ts
+++ src/app/store.ts
@@ -7,12 +7,9 @@ export const store = configureStore({
   }
 })
 
-
-        export type AppDispatch = typeof store.dispatch
-    export type RootState = ReturnType<typeof store.getState>
-
-
-    export type AppThunk<ReturnType = void> = ThunkAction<
+export type AppDispatch = typeof store.dispatch
+export type RootState = ReturnType<typeof store.getState>
+export type AppThunk<ReturnType = void> = ThunkAction<
 ReturnType,
 RootState,
 unknown,
